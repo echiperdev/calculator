57b6cffce19bea9d2f9d83f4843be24b0fc80a20
emilianchiper <123456789+echiperdev.noreply.github.com>
2023-06-28 18:18:32 +0300

    Create calculator object constructor function

--- calculator.js
+++ calculator.js
@@ -7,3 +7,14 @@ const deleteButton = document.getElementById('delete');
 const equalsButton = document.getElementById('.equals');
 const previousOperandElement = document.getElementById('previous-operand');
 const currentOperandElement = document.getElementById('current-operand');
+
+//CALCULATOR
+/* @Object Constructor Function
+/* Stores calculator object properties */
+function Calculator(previousOperandElement, currentOperandElement) {
+    this.previousOperandElement = previousOperandElement;
+    this.currentOperandElement = currentOperandElement;
+    this.previousOperand = '';
+    this.currentOperand = '';
+    this.operations = undefined;
+}
